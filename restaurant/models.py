271527fabe48a48ab6263626dc5cbf25955c3f29
Maksym Operchuk <operlast529@gmail.com>
2024-05-24 01:04:32 +0300

    Create: Restaurant model in restaurant/models.py file.

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models import UniqueConstraint
+
+
+class Restaurant(models.Model):
+    """Restaurant model."""
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 
 # Create your models here.
