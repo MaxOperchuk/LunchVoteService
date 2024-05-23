4223d6d8cf8a71269332a652befa4fe74c16e138
Maksym Operchuk <operlast529@gmail.com>
2024-05-24 01:21:29 +0300

    Create: CreateEmployeeView in employee/views.py file.

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from employee.serializers import EmployeeSerializer, AuthTokenSerializer
+
+
+class CreateEmployeeView(generics.CreateAPIView):
+    serializer_class = EmployeeSerializer
+    permission_classes = ()
 
 # Create your views here.
