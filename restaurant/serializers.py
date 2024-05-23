bc6b5d8faec27f77910c278266f0f2e2882b99b4
Maksym Operchuk <operlast529@gmail.com>
2024-05-24 01:01:41 +0300

    Create: MenuCreateSerializer in restaurant/serializers.py file.

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -38,3 +38,44 @@ class MenuSerializer(serializers.ModelSerializer):
         )
 
 
+class MenuCreateSerializer(serializers.ModelSerializer):
+    """Menu create serializer."""
+
+    restaurant = serializers.CharField(
+        source="restaurant.name",
+        read_only=True,
+    )
+    restaurant_id = serializers.PrimaryKeyRelatedField(
+        queryset=Restaurant.objects.all(),
+        source="restaurant",
+        write_only=True,
+    )
+
+    class Meta:
+        model = Menu
+        fields = (
+            "id",
+            "restaurant",
+            "restaurant_id",
+            "date",
+            "items",
+        )
+        read_only_fields = (
+            "id",
+            "restaurant",
+        )
+
+    def create(self, validated_data):
+        """
+           Create a new Menu instance.
+
+           This method creates a new Menu instance using the validated data provided.
+           It retrieves the associated Restaurant instance from the validated data,
+           creates a new Menu object, and returns it.
+        """
+
+        with transaction.atomic():
+            restaurant = validated_data.pop("restaurant")
+            restaurant = Restaurant.objects.get(id=restaurant.id)
+            menu = Menu.objects.create(restaurant=restaurant, **validated_data)
+            return menu
