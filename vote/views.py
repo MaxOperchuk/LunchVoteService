69491c8645a9fc26d51c83c94c4154338a2d8b09
Maksym Operchuk <operlast529@gmail.com>
2024-05-24 01:15:41 +0300

    Create: VoteViewSet in vote/views.py file.

--- vote/views.py
+++ vote/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.utils import timezone
+from rest_framework import status, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
+
+from vote.models import Vote
+from vote.serializers import VoteSerializer, VoteCreateSerializer
+
+
+class VoteViewSet(GenericViewSet, mixins.CreateModelMixin, mixins.ListModelMixin):
+
+    queryset = Vote.objects.select_related("employee", "menu")
+    serializer_class = VoteSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_serializer_class(self):
+        
+        if self.action == "create":
+            return VoteCreateSerializer
+
+        return self.serializer_class
+
 
 # Create your views here.
