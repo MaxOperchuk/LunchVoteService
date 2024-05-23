6485a1847676a88123a84681465103328e726651
Maksym Operchuk <operlast529@gmail.com>
2024-05-24 00:53:06 +0300

    Create: RestaurantListCreateView class in restaurant/views.py file.

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.utils import timezone
+from rest_framework import generics, status, mixins
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from restaurant.models import Restaurant, Menu
+from restaurant.permissions import IsAdminOrIfAuthenticatedReadOnly
+from restaurant.serializers import (
+    RestaurantSerializer,
+    MenuSerializer,
+    MenuCreateSerializer,
+)
+class RestaurantListCreateView(generics.ListCreateAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
 
 # Create your views here.
