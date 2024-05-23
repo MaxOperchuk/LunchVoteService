31903deddff0ad798692d1a6bdb82dd61bffcc26
Maksym Operchuk <operlast529@gmail.com>
2024-05-24 01:02:46 +0300

    Create: restaurant/urls.py file and add urlpatterns there.

---
+++ restaurant/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from restaurant.views import (
+    RestaurantListCreateView,
+    MenuListCreateView,
+)
+
+
+urlpatterns = [
+    path(
+        "restaurants/",
+        RestaurantListCreateView.as_view(),
+        name="restaurant-list-create",
+    ),
+    path("menus/", MenuListCreateView.as_view(), name="menu-list-create"),
+]
+
+app_name = "restaurant"
