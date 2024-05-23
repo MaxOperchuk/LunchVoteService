619ac10beda660d5fbf95432275b773f16701b4d
Maksym Operchuk <operlast529@gmail.com>
2024-05-24 00:57:22 +0300

    Delete: MenuCreateView & CurrentDayMenuView classes from restaurant/views.py file.

--- restaurant/views.py
+++ restaurant/views.py
@@ -19,30 +19,6 @@ class RestaurantListCreateView(generics.ListCreateAPIView):
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
-class MenuCreateView(generics.CreateAPIView):
-    queryset = Menu.objects.select_related("restaurant")
-    serializer_class = MenuSerializer
-    permission_classes = (IsAdminUser,)
-
-    def get_serializer_class(self):
-
-        if self.action == "create":
-            return MenuCreateSerializer
-
-        return self.serializer_class
-
-
-class CurrentDayMenuView(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def get(self, request, *args, **kwargs):
-        today = timezone.now().date()
-        menus = Menu.objects.filter(date=today)
-        serializer = MenuSerializer(menus, many=True)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
 class MenuListCreateView(
     generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin
 ):
