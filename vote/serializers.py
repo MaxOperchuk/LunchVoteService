4c5e0c8c417c0043acfeb9523d8a109ecf1a1e18
Maksym Operchuk <operlast529@gmail.com>
2024-05-24 01:14:02 +0300

    Create: VoteCreateSerializer in vote/serializer.py file.

--- vote/serializers.py
+++ vote/serializers.py
@@ -30,3 +30,65 @@ class VoteSerializer(serializers.ModelSerializer):
         read_only_fields = ("id",)
 
 
+class VoteCreateSerializer(VoteSerializer):
+    """Vote create serializer."""
+
+    employee = serializers.CharField(
+        source="employee.email",
+        read_only=True,
+    )
+
+    menu_id = serializers.PrimaryKeyRelatedField(
+        queryset=Menu.objects.all(),
+        source="menu",
+        write_only=True,
+    )
+
+    class Meta:
+        model = Vote
+        fields = (
+            "id",
+            "employee",
+            "menu",
+            "menu_id",
+            "voted_at",
+        )
+        read_only_fields = ("id", "employee", "menu")
+
+    def create(self, validated_data):
+        """Create a new vote instance."""
+
+        with transaction.atomic():
+            menu = validated_data.pop("menu")
+            menu = Menu.objects.get(id=menu.id)
+
+            employee = Employee.objects.get(id=self.context["request"].user.id)
+
+            vote = Vote.objects.create(
+                employee=employee,
+                menu=menu,
+                **validated_data,
+            )
+
+            return vote
+
+    def validate(self, attrs):
+        """
+        Validate that the vote for this employee
+        and menu on the voted_at date is unique.
+        """
+
+        data = super(VoteSerializer, self).validate(attrs=attrs)
+        employee = self.context["request"].user
+        menu = attrs["menu"]
+        today = timezone.now().date()
+
+        if Vote.objects.filter(
+                employee_id=employee.id, voted_at=today
+        ).exists():
+
+            raise serializers.ValidationError(
+                "You have already voted menu today."
+            )
+
+        return data
