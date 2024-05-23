9a3a1569975d4f95b6a8293dc530fcbff380e349
Maksym Operchuk <operlast529@gmail.com>
2024-05-24 00:55:48 +0300

    Create: MenuListCreateView class in restaurant/views.py file.

--- restaurant/views.py
+++ restaurant/views.py
@@ -35,7 +35,6 @@ class MenuCreateView(generics.CreateAPIView):
 class CurrentDayMenuView(APIView):
     permission_classes = (IsAuthenticated,)
 
-# Create your views here.
     def get(self, request, *args, **kwargs):
         today = timezone.now().date()
         menus = Menu.objects.filter(date=today)
@@ -44,3 +43,25 @@ class CurrentDayMenuView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+class MenuListCreateView(
+    generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin
+):
+    queryset = Menu.objects.select_related("restaurant")
+    serializer_class = MenuSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    def get_serializer_class(self):
+
+        if self.request.method == "POST":
+            return MenuCreateSerializer
+
+        return MenuSerializer
+
+    def get(self, request, *args, **kwargs):
+        today = timezone.now().date()
+        menus = Menu.objects.filter(date=today)
+        serializer = self.get_serializer(menus, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
