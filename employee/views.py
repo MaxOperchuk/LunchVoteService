fc11ce75b456994f1339ed6ca6b4b94117de8afb
Maksym Operchuk <operlast529@gmail.com>
2024-05-24 01:22:01 +0300

    Create: ManageEmployeeView in employee/views.py file.

--- employee/views.py
+++ employee/views.py
@@ -9,10 +9,14 @@ class CreateEmployeeView(generics.CreateAPIView):
     serializer_class = EmployeeSerializer
     permission_classes = ()
 
-# Create your views here.
 
 class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
     serializer_class = AuthTokenSerializer
 
 
+class ManageEmployeeView(generics.RetrieveUpdateAPIView):
+    serializer_class = EmployeeSerializer
+
+    def get_object(self):
+        return self.request.user
